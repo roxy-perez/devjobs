4bfcad4c6de233e091482ab41780d75d8a3f50f1
roxy.perez <necropsia32@gmail.com>
2024-02-16 22:49:56 +0000

    Limmiting guest and auth for Vacancies

--- resources/views/livewire/show-vacancy.blade.php
+++ resources/views/livewire/show-vacancy.blade.php
@@ -4,7 +4,7 @@
             {{ $vacancy->title }}
         </h3>
 
-        <div class="md:grid md:grid-cols-2 bg-fuchsia-100 p-4 my-10">
+        <div class="md:grid md:grid-cols-2 bg-fuchsia-100 p-4 my-10 rounded-2xl">
             <p class="font-bold text-sm uppercase text-indigo-600 my-3">{{ __('Company') }}: <span
                     class="text-gray-800 normal-case font-normal">{{ $vacancy->company }}</span></p>
             <p class="font-bold text-sm uppercase text-indigo-600 my-3">{{ __('Last Day To Apply') }}: <span
@@ -21,9 +21,16 @@
             <img src="{{ asset('storage/vacancies/' . $vacancy->image) }}" alt="{{ $vacancy->title }}">
         </div>
         <div class="md:col-span-4">
-            <h2 class="text-2xl font-bold mb-5">Descripción del Puesto</h2>
-            <p>{{ $vacancy->description }}</p>
+            <h2 class="text-2xl font-bold mb-5 dark:text-indigo-600">Descripción del Puesto</h2>
+            <p class="text-xl font-mono text-fuchsia-500 dark:text-gray-200">{{ $vacancy->description }}</p>
         </div>
     </div>
-
+    @guest
+        <div class="mt-5 bg-gray-50 border border-dashed p-5 text-center rounded-xl dark:bg-slate-600">
+            <p class="text-sm font-mono text-gray-700 dark:text-gray-300">
+                {{ __('Would you like to apply for this vacancy?') }} <a class="font-bold text-indigo-600 dark:text-indigo-400"
+                    href="{{ route('register') }}">{{ __('Get an account and apply for this and other vacancies.') }}</a>
+            </p>
+        </div>
+    @endguest
 </div>
